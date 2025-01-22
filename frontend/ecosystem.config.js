ec86eaf3b253260884b100f5b77ac08b3221917b
Dayvid <dayvid7santos2002@outlook.com>
2025-01-22 13:41:16 -0300

    feat: add PM2 configuration for production deployment

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "LC Tech Frontend",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
