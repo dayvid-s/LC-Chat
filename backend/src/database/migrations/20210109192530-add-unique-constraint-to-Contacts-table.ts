73ae7e6054692e168ad750db16e6f63804cff71d
Dayvid <dayvid7santos2002@outlook.com>
2025-01-28 14:48:12 -0300

    feat: update core again

--- backend/src/database/migrations/20210109192530-add-unique-constraint-to-Contacts-table.ts
+++ backend/src/database/migrations/20210109192530-add-unique-constraint-to-Contacts-table.ts
@@ -1,19 +1,11 @@
 import { QueryInterface } from "sequelize";
 
 module.exports = {
-  up: async (queryInterface: QueryInterface) => {
-    const [results] = await queryInterface.sequelize.query(`
-      SELECT conname
-      FROM pg_constraint
-      WHERE conname = 'number_companyid_unique';
-    `);
-
-    if (results.length === 0) {
-      return queryInterface.addConstraint("Contacts", ["number", "companyId"], {
-        type: "unique",
-        name: "number_companyid_unique"
-      });
-    }
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.addConstraint("Contacts", ["number", "companyId"], {
+      type: "unique",
+      name: "number_companyid_unique"
+    });
   },
 
   down: (queryInterface: QueryInterface) => {
