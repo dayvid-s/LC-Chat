da8b14348b36c9b81c81dad17deb97519dbb475e
Dayvid <dayvid7santos2002@outlook.com>
2025-02-03 16:02:42 -0300

    feat: removed support for ticketz

--- whaticket/frontend/src/pages/Dashboard/index.js
+++ whaticket/frontend/src/pages/Dashboard/index.js
@@ -1,59 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Grid from "@material-ui/core/Grid";
 import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Paper from "@material-ui/core/Paper";
 import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import Typography from "@material-ui/core/Typography";
 
 // ICONS
-import SpeedIcon from "@material-ui/icons/Speed";
-import GroupIcon from "@material-ui/icons/Group";
-import AssignmentIcon from "@material-ui/icons/Assignment";
-import PersonIcon from "@material-ui/icons/Person";
-import TodayIcon from '@material-ui/icons/Today';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import CallIcon from "@material-ui/icons/Call";
-import RecordVoiceOverIcon from "@material-ui/icons/RecordVoiceOver";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import GroupAddIcon from "@material-ui/icons/GroupAdd";
 import HourglassEmptyIcon from "@material-ui/icons/HourglassEmpty";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import ForumIcon from "@material-ui/icons/Forum";
-import FilterListIcon from "@material-ui/icons/FilterList";
-import ClearIcon from "@material-ui/icons/Clear";
-import SendIcon from '@material-ui/icons/Send';
-import MessageIcon from '@material-ui/icons/Message';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import TimerIcon from '@material-ui/icons/Timer';
 
+import { blue, grey } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core/styles";
-import { grey, blue } from "@material-ui/core/colors";
 import { toast } from "react-toastify";
 
-import Chart from "./Chart";
 import ButtonWithSpinner from "../../components/ButtonWithSpinner";
+import Chart from "./Chart";
 
-import CardCounter from "../../components/Dashboard/CardCounter";
-import TableAttendantsStatus from "../../components/Dashboard/TableAttendantsStatus";
 import { isArray } from "lodash";
+import TableAttendantsStatus from "../../components/Dashboard/TableAttendantsStatus";
 
-import useDashboard from "../../hooks/useDashboard";
 import useCompanies from "../../hooks/useCompanies";
+import useDashboard from "../../hooks/useDashboard";
 
 import { isEmpty } from "lodash";
 import moment from "moment";
-import { i18n } from "../../translate/i18n";
-import OnlyForSuperUser from "../../components/OnlyForSuperUser";
-import useAuth from "../../hooks/useAuth.js";
-import clsx from "clsx";
 import { loadJSON } from "../../helpers/loadJSON";
+import useAuth from "../../hooks/useAuth.js";
 
-import TicketzRegistry from "../../components/TicketzRegistry";
-import config from "../../services/config";
+// import TicketzRegistry from "../../components/TicketzRegistry";
 import api from "../../services/api.js";
 
 const gitinfo = loadJSON('/gitinfo.json');
@@ -272,35 +256,35 @@ const Dashboard = () => {
   const { find } = useDashboard();
   const { finding } = useCompanies();
   const { getCurrentUserInfo } = useAuth();
-    
+
   const [supportBoxOpen, setSupportBoxOpen] = useState(false);
   const [registered, setRegistered] = useState(false);
   const [proInstructionsOpen, setProInstructionsOpen] = useState(false);
-  
+
   async function showProInstructions() {
     if (gitinfo.commitHash) {
       setProInstructionsOpen(true);
       return;
     }
-    
+
     window.open("https://pro.ticke.tz", "_blank");
   }
-  
+
   useEffect(() => {
     getCurrentUserInfo().then(
       (user) => {
         setCurrentUser(user);
       }
     );
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(async () => {
     const registry = await api.get("/ticketz/registry");
 
-    setRegistered( registry?.data?.disabled || !!(registry?.data?.whatsapp ) );
+    setRegistered(registry?.data?.disabled || !!(registry?.data?.whatsapp));
   }, []);
-    
+
   useEffect(() => {
     async function firstLoad() {
       await fetchData();
@@ -464,145 +448,15 @@ const Dashboard = () => {
       </div>
     );
   }
-      
+
   return (
     <div>
       <Container maxWidth="lg" className={classes.container}>
         <Grid container spacing={3} justifyContent="flex-end">
 
-          <OnlyForSuperUser
-            user={currentUser}
-            yes={() => (
-              <Grid item xs={12}>
-                {!registered &&
-                  <Paper className={classes.ticketzRegistryPaper}>
-                    <TicketzRegistry onRegister={setRegistered} />
-                  </Paper>
-                }
-                <Paper className={clsx(classes.ticketzProPaper, {
-                  [classes.clickpointer]: !proInstructionsOpen,
-                })} onClick={() => showProInstructions()}>
-                  <Grid container justifyContent="flex-end">
-                    <Grid className={classes.ticketzProBox} item xs={12} md={proInstructionsOpen ? 4 : 6} sm={12}>
-                      <div>
-                        <img className={classes.ticketzProScreen} src="https://pro.ticke.tz/images/0/7/3/0/b/0730b234af7b4b0dac72d09828863bb7cb9193ea-ticketz-computador.png" />
-                      </div>
-                    </Grid>
-                    { !proInstructionsOpen &&
-                    <Grid className={classes.ticketzProBox} item xs={12} md={6} sm={12}>
-                      <Typography className={classes.ticketzProTitle} component="h3" variant="h5" gutterBottom>
-                        Ticketz PRO
-                      </Typography>
-                      <Typography component="h3" variant="h7" gutterBottom>
-                      <ul className={classes.ticketzProFeatures}>
-                        <li>Funcionalidades exclusivas</li>
-                        <li>Suporte Avançado</li>
-                        <li>Migração Facilitada</li>
-                      </ul>
-                      </Typography>
-                      <Typography component="h3" variant="h5">
-                        Assine por R$ 199/mês
-                      </Typography>
-                      <Typography component="h3" variant="h7" gutterBottom>
-                        direto dentro do sistema
-                      </Typography>
-                      { gitinfo.commitHash && 
-                      <Typography component="h3" variant="h5">
-                        Clique para instruções de Upgrade
-                      </Typography>
-                      }
-                      { !gitinfo.commitHash && 
-                      <Typography component="h3" variant="h5">
-                        Clique para visitar o site!
-                      </Typography>
-                      }
-                    </Grid>
-                    }
-                    { proInstructionsOpen &&
-                    <Grid className={classes.ticketzProBox} item xs={12} md={8} sm={12}>
-                      <Typography className={classes.ticketzProTitle} component="h3" variant="h5" gutterBottom>
-                        Instruções de Upgrade
-                      </Typography>
-                      <Typography paragraph>
-                        Se você instalou as imagens disponibilizadas pelo projeto em um
-                        servidor ou VPS utilizando as instruções facilitadas tudo o que
-                        você precisa fazer é acessar seu servidor e digitar o comando abaixo:
-                      </Typography>
-                      <Typography className={classes.ticketzProCommand} paragraph>
-                        curl -sSL update.ticke.tz | sudo bash -s pro
-                      </Typography>
-                      <Typography paragraph>
-                        Em instantes o Ticketz PRO estará instalado com todos os teus dados,
-                        agora só precisa ir até o menu de usuário, clicar em "Assinatura do
-                        Ticketz PRO" e fazer a sua assinatura.
-                      </Typography>
-                      <Typography paragraph>
-                        Se a tua instalação for diferente ou acredita que precisa
-                        de auxílio para instalar o Ticketz
-                        Pro, <a href="https://wa.me/554935670707"> entre
-                        em contato</a> que nós ajudamos!
-                      </Typography>
-                    </Grid>
-                    }
-                  </Grid>
-                </Paper>
-              </Grid>
-            )} />
-
-
-          <OnlyForSuperUser
-            user={currentUser}
-            yes={() => (
-              <Grid item xs={12}>
-                <Paper className={clsx(classes.supportPaper, {
-                  [classes.clickpointer]: !supportBoxOpen,
-                })} onClick={() => setSupportBoxOpen(true)}>
-                  <Typography component="h2" variant="h6" gutterBottom>
-                    {i18n.t("ticketz.support.title")}
-                  </Typography>
-                  { supportBoxOpen &&
-                  <Grid container justifyContent="flex-end">
-                    <Grid className={classes.supportBox} item xs={12} md={4} sm={12}>
-                      <Typography component="h3" variant="h6" gutterBottom>
-                        PIX
-                      </Typography>
-                      <div>
-                        <img className={classes.paymentpix} src="/ticketzpix.png" />
-                      </div>
-                      <Typography className={classes.pixkey} component="body2" paragraph>
-                        1ab11506-9480-4303-8e1e-988e7c49ed4d
-                      </Typography>
-                    </Grid>
-                    <Grid className={classes.supportBox} item xs={12} md={4} sm={12}>
-                      <Typography component="h3" variant="h6" gutterBottom>
-                        {i18n.t("ticketz.support.mercadopagotitle")}
-                      </Typography>
-                      <Typography component="body2" paragraph>
-                        {i18n.t("ticketz.support.recurringbrl")}
-                      </Typography>
-                      <div><a href="https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c9380848f1b8ed1018f2b011f90061f" target="_blank">
-                        <img className={classes.paymentimg} src="/mercadopago.png" />
-                      </a></div>
-                    </Grid>
-                    <Grid className={classes.supportBox} item xs={12} md={4} sm={12}>
-                      <Typography component="h3" variant="h6" gutterBottom>
-                        {i18n.t("ticketz.support.paypaltitle")}
-                      </Typography>
-                      <Typography component="body2" paragraph>
-                        {i18n.t("ticketz.support.international")}
-                      </Typography>
-                      <div><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=X6XHVCPMRQEL4" target="_blank">
-                        <img className={classes.paymentimg} src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" />
-                      </a></div>
-                    </Grid>
-                  </Grid>
-                  }
-                </Paper>
-              </Grid>
-            )} />
 
           {/* DASHBOARD ATENDIMENTOS HOJE */}
-          
+
           {/* GRID DO VENCIMENTO */}
           {/* <Grid item xs={12} sm={6} md={3}>
             <CardCounter
