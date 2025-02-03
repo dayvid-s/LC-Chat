722b741d4ff86dfea82e7b97ca4fb0b992813edf
Dayvid <dayvid7santos2002@outlook.com>
2025-02-03 17:54:18 -0300

    feat: add pm2 configuration

---
+++ whaticket/frontend/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "LC Chat Frontend",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
