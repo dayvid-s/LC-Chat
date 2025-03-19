db675ae0e1d7adae9a3094f0388ee90206146522
“Dayvid” <dayvid7santos2002@outlook.com>
2025-03-18 23:00:17 -0300

    feat: Add PM2 support for process management and create ecosystem configuration

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "LC Chat Backend",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
