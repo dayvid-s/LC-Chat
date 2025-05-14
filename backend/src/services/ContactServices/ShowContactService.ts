76aaae2a90581b62f933f1c54b1383c511428fb0
“Dayvid” <dayvid7santos2002@outlook.com>
2025-05-13 22:21:10 -0300

    feat: comment validation of deletion of records from other companies in contact and scheduling services

--- backend/src/services/ContactServices/ShowContactService.ts
+++ backend/src/services/ContactServices/ShowContactService.ts
@@ -7,9 +7,9 @@ const ShowContactService = async (
 ): Promise<Contact> => {
   const contact = await Contact.findByPk(id, { include: ["extraInfo"] });
 
-  if (contact?.companyId !== companyId) {
-    throw new AppError("Não é possível excluir registro de outra empresa");
-  }
+  // if (contact?.companyId !== companyId) {
+  //   throw new AppError("Não é possível excluir registro de outra empresa");
+  // }
 
   if (!contact) {
     throw new AppError("ERR_NO_CONTACT_FOUND", 404);
